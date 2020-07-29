6756c7ed060e18f09a65082b737ebe6c4c125bab
Skittlez1522 <61472875+Skittlez1522@users.noreply.github.com>
2020-07-28 19:41:52 -0600

    Add files via upload

---
+++ hack.js
@@ -0,0 +1,4 @@
+GET /campgrounds/devils-tower/reserve/select-site/?sitecategoryid=R&sitetypeid=13319&checkInDate=7/29/2020&checkOutDate=7/30/2020&adults=1&kids=0&kidsFree=0&pets=No&equipmentType=PC&equipmentlength=20&slideouts=No&vkrnumber=&vkrpostalcode=&hotdealcode=&purchasevkr=NotSelected&popup=true;
+
+document.cookie = "ASP.NETSessionId=ky2mgngdg3k0igefbenrcnp0";
+document.cookie = "_RequestVerificationToken=j44mIBLcuI21v5SfyOyl2za_e_ouO_rsJ-o7W1JAwdflNIpT-F_k4e0MpuQKUze4ERwqpdzuo10ZxJYvXUD1aeOkayg1";
